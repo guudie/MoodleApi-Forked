bd3d3e42c6fa112630958020fd8642f81862199e
vinhvx <vinhvx3@fpt.com.vn>
2021-10-21 17:14:56 +0700

    fix cores

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("./src/modules/connection");
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 let AuthRoute = require("./src/routes/AuthRoute");
 
