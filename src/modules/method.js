2319a8e898b58c9db91e1f47c2f58b5db9bf9f0d
DESKTOP-5EON1NH\XV11 <vinhvx3@fpt.com.vn>
2022-01-16 10:33:19 +0700

    Fix img

--- src/modules/method.js
+++ src/modules/method.js
@@ -1,7 +1,11 @@
 module.exports = {
   getImgDrive(url) {
-    return url
-      ? `https://drive.google.com/uc?export=view&id=${url.split("/")[5]}`
-      : "";
+    if (url) {
+      if (url.includes("drive.google.com"))
+        return `https://drive.google.com/uc?export=view&id=${
+          url.split("/")[5]
+        }`;
+      else return url;
+    } else return "";
   },
 };
